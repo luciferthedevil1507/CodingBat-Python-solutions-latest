50deacae25f62214814a648af015031a19b72258
luciferthedevil1507 <66934872+luciferthedevil1507@users.noreply.github.com>
2020-09-14 00:56:49 +0530

    Update Warmup1.py

--- Warmup1.py
+++ Warmup1.py
@@ -1,4 +1,4 @@
-sleep_in
+#sleep_in
 #The parameter weekday is True if it is a weekday, and the parameter vacation is True if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return True if we sleep in.
 #sleep_in(False, False) → True
 #sleep_in(True, False) → False
@@ -8,3 +8,15 @@ def sleep_in(weekday, vacation):
     return True
   else:
     return False
+
+#monkey_trouble
+#We have two monkeys, a and b, and the parameters a_smile and b_smile indicate if each is smiling. We are in trouble if they are both smiling or if neither of them is smiling. Return True if we are in trouble.
+#monkey_trouble(True, True) → True
+#monkey_trouble(False, False) → True
+#monkey_trouble(True, False) → False
+def monkey_trouble(a_smile, b_smile):
+  if a_smile and b_smile:
+    return True
+  if not a_smile and not b_smile:
+    return True
+  return False
