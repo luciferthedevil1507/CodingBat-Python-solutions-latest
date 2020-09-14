b040f67c760bc08946e0299ecbd9a8441bbef6c9
luciferthedevil1507 <66934872+luciferthedevil1507@users.noreply.github.com>
2020-09-14 18:19:06 +0530

    Update Warmup1.py

--- Warmup1.py
+++ Warmup1.py
@@ -71,4 +71,13 @@ def near_hundred(n):
     return True
   else:
     return False
- 
+#pos_neg 
+#Given 2 int values, return True if one is negative and one is positive. Except if the parameter "negative" is True, then return True only if both are negative.
+#pos_neg(1, -1, False) → True
+#pos_neg(-1, 1, False) → True
+#pos_neg(-4, -5, True) → True
+def pos_neg(a, b, negative):
+  if negative:
+    return (a < 0 and b < 0)
+  else:
+    return ((a < 0 and b > 0) or (a > 0 and b < 0))
