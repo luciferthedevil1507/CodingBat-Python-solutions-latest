1b736a91e28b9e4fa364ac09ae3746f020da9ff9
luciferthedevil1507 <66934872+luciferthedevil1507@users.noreply.github.com>
2020-09-16 11:21:30 +0530

    Update Warmup1.py

--- Warmup1.py
+++ Warmup1.py
@@ -101,3 +101,15 @@ def missing_char(str, n):
   start = str[:n]
   end = str[n+1:]
   return start+end
+
+#front_back
+#Given a string, return a new string where the first and last chars have been exchanged.
+#front_back('code') → 'eodc'
+#front_back('a') → 'a'
+#front_back('ab') → 'ba'
+def front_back(str):
+  if len(str) <=1:
+    return str
+  mid = str[1:len(str)-1]
+  return str[-1] + mid + str[0]
+
