a96c94f3f2dba263373618bbbbaf365cc44ccda9
luciferthedevil1507 <66934872+luciferthedevil1507@users.noreply.github.com>
2020-09-19 22:38:32 +0530

    Update String1.py

--- String1.py
+++ String1.py
@@ -5,3 +5,29 @@
 #hello_name('X') → 'Hello X!'
 def hello_name(name):
   return 'Hello ' + name+'!'
+
+#make_abba
+#Given two strings, a and b, return the result of putting them together in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
+#make_abba('Hi', 'Bye') → 'HiByeByeHi'
+#make_abba('Yo', 'Alice') → 'YoAliceAliceYo'
+#make_abba('What', 'Up') → 'WhatUpUpWhat
+def make_abba(a, b):
+  return a+b+b+a
+
+#make_tags
+#The web is built with HTML strings like "<i>Yay</i>" which draws Yay as italic text. In this example, the "i" tag makes <i> and </i> which surround the word "Yay". Given tag and word strings, create the HTML string with tags around the word, e.g. "<i>Yay</i>".
+#make_tags('i', 'Yay') → '<i>Yay</i>'
+#make_tags('i', 'Hello') → '<i>Hello</i>'
+#make_tags('cite', 'Yay') → '<cite>Yay</cite>'
+def make_tags(tag, word):
+  return "<"+tag+">"+word+"</"+tag+">"
+
+#make_out_word
+#Given an "out" string length 4, such as "<<>>", and a word, return a new string where the word is in the middle of the out string, e.g. "<<word>>".
+#make_out_word('<<>>', 'Yay') → '<<Yay>>'
+#make_out_word('<<>>', 'WooHoo') → '<<WooHoo>>'
+#make_out_word('[[]]', 'word') → '[[word]]'
+def make_out_word(out, word):
+  return out[:2]+word+out[2:]
+
+#
