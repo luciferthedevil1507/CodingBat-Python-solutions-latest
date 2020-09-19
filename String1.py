fd92df07a38c866d5d0b78838f20eacd8c2b52bd
luciferthedevil1507 <66934872+luciferthedevil1507@users.noreply.github.com>
2020-09-19 22:51:21 +0530

    Update String1.py

--- String1.py
+++ String1.py
@@ -30,4 +30,19 @@ def make_tags(tag, word):
 def make_out_word(out, word):
   return out[:2]+word+out[2:]
 
-#
+#extra_end
+#Given a string, return a new string made of 3 copies of the last 2 chars of the original string. The string length will be at least 2.
+#extra_end('Hello') → 'lololo'
+#extra_end('ab') → 'ababab'
+#extra_end('Hi') → 'HiHiHi'
+def extra_end(str):
+  lt = str[-2:]
+  return lt+lt+lt
+
+#first_two
+#Given a string, return the string made of its first two chars, so the String "Hello" yields "He". If the string is shorter than length 2, return whatever there is, so "X" yields "X", and the empty string "" yields the empty string "".
+#first_two('Hello') → 'He'
+#first_two('abcdefg') → 'ab'
+#first_two('ab') → 'ab'
+def first_two(str):
+  return str[:2]
