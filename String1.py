00b440a78236d75e4c7b6033b90b7cc81d7ebd84
luciferthedevil1507 <66934872+luciferthedevil1507@users.noreply.github.com>
2020-09-19 23:00:38 +0530

    Update String1.py

--- String1.py
+++ String1.py
@@ -56,3 +56,12 @@ def first_half(str):
   lstr = len(str)
   hlstr = lstr//2
   return str[:hlstr]
+
+#without_end
+#
+#Given a string, return a version without the first and last char, so "Hello" yields "ell". The string length will be at least 2.
+#without_end('Hello') → 'ell'
+#without_end('java') → 'av'
+#without_end('coding') → 'odin'
+def without_end(str):
+  return str[1:len(str)-1]
