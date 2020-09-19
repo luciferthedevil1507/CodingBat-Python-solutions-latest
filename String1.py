0b13932cc548cb77d41b7c1890784d94e4854bba
luciferthedevil1507 <66934872+luciferthedevil1507@users.noreply.github.com>
2020-09-19 23:17:39 +0530

    Update String1.py

--- String1.py
+++ String1.py
@@ -65,3 +65,30 @@ def first_half(str):
 #without_end('coding') → 'odin'
 def without_end(str):
   return str[1:len(str)-1]
+#combo_string
+#Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty (length 0).
+#combo_string('Hello', 'hi') → 'hiHellohi'
+#combo_string('hi', 'Hello') → 'hiHellohi'
+#combo_string('aaa', 'b') → 'baaab'
+def combo_string(a, b):
+  if len(a) < len(b):
+    return a+b+a
+  else:
+    return b+a+b
+#non_start
+#Given 2 strings, return their concatenation, except omit the first char of each. The strings will be at least length 1.
+#non_start('Hello', 'There') → 'ellohere'
+#non_start('java', 'code') → 'avaode'
+#non_start('shotl', 'java') → 'hotlava'
+def non_start(a, b):
+  return a[1:]+b[1:]
+
+#left2
+#Given a string, return a "rotated left 2" version where the first 2 chars are moved to the end. The string length will be at least 2.
+#left2('Hello') → 'lloHe'
+#left2('java') → 'vaja'
+#left2('Hi') → 'Hi'
+def left2(str):
+  return str[2:]+str[:2]
+
+
