6f2e8bbddd995f3e9cc71e1d01698a93f3100f91
luciferthedevil1507 <66934872+luciferthedevil1507@users.noreply.github.com>
2020-09-28 02:29:16 +0530

    Update Warmup-2.py

--- Warmup-2.py
+++ Warmup-2.py
@@ -5,3 +5,57 @@
 #string_times('Hi', 1) → 'Hi'
 def string_times(str, n):
   return str*n
+
+#front_times
+#Given a string and a non-negative int n, we'll say that the front of the string is the first 3 chars, or whatever is there if the string is less than length 3. Return n copies of the front;
+#front_times('Chocolate', 2) → 'ChoCho'
+#front_times('Chocolate', 3) → 'ChoChoCho'
+#front_times('Abc', 3) → 'AbcAbcAbc'
+def front_times(str, n):
+  return str[:3]*n
+
+#string_bits
+#Given a string, return a new string made of every other char starting with the first, so "Hello" yields "Hlo".
+#string_bits('Hello') → 'Hlo'
+#string_bits('Hi') → 'H'
+#string_bits('Heeololeo') → 'Hello'
+def string_bits(str):
+  string = ''
+  for i in range(len(str)):
+    if i%2 == 0:
+      string += str[i]
+  return string
+  
+  
+#string_splosion
+#Given a non-empty string like "Code" return a string like "CCoCodCode".
+#string_splosion('Code') → 'CCoCodCode'
+#string_splosion('abc') → 'aababc'
+#string_splosion('ab') → 'aab'
+def string_splosion(str):
+  result = ''
+  for i in range(len(str)):
+    result += str[:i+1]
+  return result
+
+
+# last2
+#Given a string, return the count of the number of times that a substring length 2 appears in the string and also as the last 2 chars of the string, so "hixxxhi" yields 1 (we won't count the end substring).
+#last2('hixxhi') → 1
+#last2('xaxxaxaxx') → 1
+#last2('axxxaaxx') → 2
+def last2(str):
+    last2 = str[-2:]
+    return sum(1 for i in range(len(str)-2) if str[i:i+2] == last2)
+  
+#array_count9
+#Given an array of ints, return the number of 9's in the array.
+#array_count9([1, 2, 9]) → 1
+#array_count9([1, 9, 9]) → 2
+#array_count9([1, 9, 9, 3, 9]) → 3
+def array_count9(nums):
+  count = 0
+  for num in nums:
+    if num == 9:
+      count += 1
+  return count
